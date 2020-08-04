163b42fa17bd59fd5bb761a53cb0b387a50f86dc
Chuck Stewart <cvstewart@gmail.com>
2020-08-04 15:19:41 -0400

    Writing the high-level driver and associated changes - adding new files

---
+++ db_interface_sim.py
@@ -0,0 +1,316 @@
+import networkx as nx
+
+import cluster_tools as ct
+import compare_clusterings
+import db_interface
+
+'''
+Init edges
+Init clusters
+Correct clusters
+Query edges
+'''
+
+
+class db_interface_sim(db_interface.db_interface):
+    def __init__(self, edges, clustering):
+        super().__init__()
+        self.edge_graph = nx.Graph()
+        self.add_edges(edges)
+        self.clustering = clustering
+        self.node_to_cid = ct.build_node_to_cluster_mapping(self.clustering)
+
+    def add_edges(self, quads):
+        '''
+        See base class for requirements
+        '''
+        self.edge_graph.add_edges_from([(n0, n1) for n0, n1, _, _ in quads])
+        for n0, n1, w, aug_name in quads:
+            attrib = self.edge_graph[n0][n1]
+            if aug_name == 'human':
+                if 'human' in attrib:
+                    attrib['human'].append(w)
+                else:
+                    attrib['human'] = [w]
+            else:
+                attrib[aug_name] = w
+
+    def get_weight(self, triple):
+        n0, n1, aug_name = triple
+        try:
+            attrib = self.edge_graph[n0][n1]
+            if aug_name == 'human':
+                return sum(attrib['human'])
+            else:
+                return attrib[aug_name]
+        except KeyError:
+            return None
+
+    def cluster_exists(self, cid):
+        return cid in self.clustering
+
+    def get_cid(self, node):
+        '''
+        Get the cluster id associated with a node. Returns None if the
+        node is not part of a current cluster.
+        '''
+        try:
+            return self.node_to_cid[node]
+        except KeyError:
+            return None
+
+    def get_nodes_in_cluster(self, cid):
+        '''
+        Find all the nodes the cluster referenced by cid.
+        '''
+        try:
+            return self.clustering[cid]
+        except:
+            return None
+
+    def edges_from_attributes(self, n0, n1):
+        quads = []
+        attrib = self.edge_graph[n0][n1]
+        for a, wgt in attrib.items():
+            if a == 'human':
+                quads.extend([(n0, n1, w, 'human') for w in wgt])
+            else:
+                quads.append((n0, n1, wgt, a))
+        return quads
+        
+    def edges_within_cluster(self, cid):
+        '''
+        Find the multigraph edges that are within a cluster.
+        Edges must be returned with n0<n1.   
+        '''
+        quads = []
+        cluster = sorted(self.clustering[cid])
+        for i, ni in enumerate(cluster):
+            for j in range(i + 1, len(cluster)):
+                nj = cluster[j]
+                if nj in self.edge_graph[ni]:
+                    quads.extend(self.edges_from_attributes(ni, nj))
+        return quads
+
+    def edges_leaving_cluster(self, cid):
+        quads = []
+        cluster = self.clustering[cid]
+        for ni in cluster:
+            for nj in self.edge_graph[ni]:
+                if nj not in cluster:
+                    quads.extend(self.edges_from_attributes(ni, nj))
+        return quads
+
+    def edges_between_clusters(self, cid0, cid1):
+        '''
+        Find the multigraph edges that connect between cluster cid0
+        and cluster cid1
+        '''
+        assert cid0 != cid1
+        quads = []
+        cluster1 = self.clustering[cid1]
+        for ni in self.clustering[cid0]:
+            ei = self.edge_graph[ni]
+            for nj in cluster1:
+                if nj in ei:
+                    quads.extend(self.edges_from_attributes(ni, nj))
+        return quads
+
+    def edges_between_nodes(self, node_set):
+        '''
+        Find all edges between any pair of nodes in the node set.
+        '''
+        quads = []
+        node_list = sorted(node_set)
+        for i, ni in enumerate(node_list):
+            for j in range(i+1, len(node_list)):
+                nj = node_list[j]
+                if nj in self.edge_graph[ni]:
+                    quads.extend(self.edges_from_attributes(ni, nj))
+        return quads
+
+    def edges_from_node(self, node):
+        quads = []
+        for nj in self.edge_graph[node]:
+            quads.extend(self.edges_from_attributes(node, nj))
+        return quads
+
+    def remove_nodes(self, nodes):
+        for n in nodes:
+            self.remove_node(n)
+
+    def remove_node(self, n):
+        if n not in self.edge_graph.nodes:
+            return
+
+        self.edge_graph.remove_node(n)
+        cid = self.node_to_cid[n]
+        del self.node_to_cid[n]
+        self.clustering[cid].remove(n)
+        if len(self.clustering[cid]) == 0:
+            del self.clustering[cid]
+        
+    def commit_cluster_change(self, cc):
+        '''
+        Commit the cluster changes to the database. This involves
+        updating the node to cluster id dictionary and the clustering
+        dictionary. One way to do this would be to have special
+        operations for each type of change. Instead, this function
+        works generically except for the single case of no changes at
+        all. 
+        '''
+        if cc.change_type == 'Unchanged':
+            return
+        
+        # 1. Add new clusters
+        self.clustering.update(cc.new_clustering)
+
+        # 2. Remove old clusters
+        removed_cids = set(cc.old_clustering.keys()) - set(cc.new_clustering.keys())
+        for old_c in removed_cids:
+            del self.clustering[old_c]
+
+        # 3. Update the node to clusterid mapping
+        new_node_to_cid = ct.build_node_to_cluster_mapping(cc.new_clustering)
+        self.node_to_cid.update(new_node_to_cid)
+
+        # 4. Removed nodes should have already been removed from the
+        #    db through the call to self.remove_nodes.
+        for n in cc.removed_nodes:
+            assert(n not in self.node_to_cid)
+
+
+def test_example():
+    quads = [('a', 'b', 4, 'vamp'),
+             ('a', 'b', 10, 'human'),
+             ('a', 'd', 11, 'embed'),
+             ('d', 'e', -5, 'vamp'),
+             ('a', 'e', 2, 'embed'),
+             ('b', 'c', -8, 'human'),
+             ('c', 'e', 1, 'vamp'),
+             ('c', 'e', -4, 'embed'),
+             ('a', 'b', 12, 'human'),
+             ('c', 'f', 2, 'vamp'),
+             ('d', 'e', -6, 'embed'),
+             ('d', 'g', -19, 'human'),
+             ('e', 'g', 12, 'human'),
+             ('e', 'h', 4, 'embed'),
+             ('c', 'f', 9, 'human'),
+             ('e', 'h', 5, 'vamp'),
+             ('c', 'f', 11, 'human'),
+             ('c', 'f', 6, 'vamp'),
+             ('f', 'c', 7, 'embed'),
+             ('a', 'd', 12, 'embed'),
+             ('g', 'h', 12, 'vamp'),
+             ('f', 'h', -6, 'embed')]
+    clustering = {1000: ['a', 'b', 'd'],
+                  1001: ['c', 'f'],
+                  1002: ['e', 'g', 'h']}
+    return quads, clustering
+
+
+def print_edge(g, n0, n1):
+    print("(%s, %s):" % (n0, n1))
+    for k, v in g[n0][n1].items():
+        print(k, v)
+
+
+if __name__ == "__main__":
+    quads, clustering = test_example()
+
+    db = db_interface_sim(quads, clustering)
+    print(db.clustering)
+
+    for n0 in db.edge_graph:
+        for n1 in db.edge_graph[n0]:
+            if n0 < n1:
+                print()
+                print_edge(db.edge_graph, n0, n1)
+
+    print("\nTest get weight")
+    triples = [('a', 'b', 'human'),  # 22
+               ('d', 'e', 'vamp'),   # -5
+               ('d', 'e', 'embed'),  # -6
+               ('d', 'e', 'human'),  # None
+               ('a', 'c', 'human'),  # None
+               ('a', 'q', 'embed'),  # None
+               ('m', 'z', 'vamp')]   # None
+    answers = [22, -5, -6, None, None, None, None]
+    num_err = 0
+    for t, a in zip(triples, answers):
+        res = db.get_weight(t)
+        print('Expected', a, 'and got', res)
+        if res != a:
+            print('Error')
+            num_err += 1
+    if num_err == 0:
+        print('no errors')
+
+    print("\nTest get_cid")
+    nodes = ['a', 'e', 'z']
+    answers = [1000, 1002, None]
+    num_err = 0
+    for n, a in zip(nodes, answers):
+        res = db.get_cid(n)
+        print('Expected', a, 'and got', res)
+        if res != a:
+            print('Error')
+            num_err += 1
+    if num_err == 0:
+        print('no errors')
+
+    print("\nTest get_nodes_in_cluster")
+    cids = [1000, 1003]
+    answers = [['a', 'b', 'd'], None]
+    num_err = 0
+    for cid, a in zip(cids, answers):
+        res = db.get_nodes_in_cluster(cid)
+        print('Expected', a, 'and got', res)
+        if res != a:
+            print('Error')
+            num_err += 1
+    if num_err == 0:
+        print('no errors')
+
+    print("\nTest edges_within_cluster")
+    print(db.edges_within_cluster(1000))
+
+    print("\nTest edges_leaving_cluster")
+    print(db.edges_leaving_cluster(1002))
+
+    print("\nTest edges_between_clusters")
+    print(db.edges_between_clusters(1002, 1000))
+
+    print("\nTest edges_between_nodes")
+    print(db.edges_between_nodes(set(['a', 'b', 'e', 'g'])))
+
+    print("\nTest edges_from_node a (should have edges to b, d, e")
+    print(db.edges_from_node('a'))
+    print("\nTest edges_from_node e (should have edges to a, c, d, g, h)")
+    print(db.edges_from_node('e'))
+
+    print('\nTesting remove_nodes and commit_cluster_change')
+    print('Output the current state:')
+    print(db.clustering)
+    print(db.node_to_cid)
+
+    old_clustering = {1001: set(['c', 'f']), 1002: set(['e', 'g', 'h'])}
+    new_clustering = {1002: set(['c', 'e', 'g']), 1003: set(['j']), 1004: set(['h', 'k',])}
+    cc = compare_clusterings.clustering_change(old_clustering, new_clustering)
+    print('Here is the cluster change object')
+    cc.print_it()
+
+    print('Removing the node(s)')
+    db.remove_nodes(cc.removed_nodes)
+
+    print('Here is the new state')
+    print(db.clustering)
+    print(db.node_to_cid)
+
+    db.commit_cluster_change(cc)
+    print('After committing the change, here is the final state')
+    print(db.clustering)
+    print(db.node_to_cid)
+    
+    
+    
---
+++ overall_driver.py
@@ -0,0 +1,223 @@
+import argparse
+import configparser
+import json
+import sys
+
+import ga_driver
+import db_interface_sim
+import edge_generator_sim
+
+
+'''
+This is a top-level driver for the LCA graph algorithm, written mostly
+to illustrate use of the graph algorithm through small examples and
+simulations. Of particular note there are two objects created here
+that must be replaced by objects that are connected to "real"
+information about animals:  the edge and id database, and the
+generator for edges through calls to verifiers or calls to human
+reviewers.
+
+Three key files are needed here:
+
+1. The configuration file.  See the config.ini example
+
+2. The json file of recent verifier ground truth positive and negative
+probability results. Note as a reminder that both the values of the
+probabilities AND the relative fraction of positive and negative
+ground truth samples are important here. 
+
+3. The request json file, which includes the simulated database, the
+simulated edge generator and the actual request.  See
+request_example.json
+
+Note that the first two will always be needed even if this is running
+"for real", as will the actual request.  So in an non-simulation, only
+the database and edge generator object need to be replaced.
+'''
+
+
+'''
+1. Rerun tests on graph algorithm.  Check.
+07-29:  Done. Needed to adjust and set some parameters by hand.
+Needed to handle node_set or a lack of them in the edge result
+callbacks.
+
+2. Review flow from overall_driver to ga_driver to graph_algorithm and
+modifying logging. Focus on where the work flow will naturally start.
+07-31: Done
+
+3. Test compare clustering.  Add documentation
+07-31: Done
+
+4. Test ga_driver. Add docoumentation
+07-31: Done with tests, but don't test call to graph algorithm; need
+simuluation.
+Start here with documentation on Monday and coordinate with
+graph_algorithm.py
+08-03: done
+
+5. Test overall_driver. Add documentation
+08/04: done
+
+6. Generate additional examples, including verification only and
+revision following review.
+
+7. Write and test commit_cluster_change in db_interface_sim.py
+08/04: done
+
+8. Push...
+ '''
+
+
+def form_database(request):
+    '''
+    From the request json object extract the database if it is there.
+    If not, return an empty database. The json includes edge quads
+    (n0, n1, w, aug_name) and a clustering dictionary.
+    '''
+    edge_quads = []
+    clustering_dict = dict()
+
+    if 'database' not in request:
+        return edge_quads, clustering_dict
+
+    req_db = request['database']
+    if 'quads' in req_db:
+        edge_quads = req_db['quads']
+    if 'clustering' in req_db:
+        clustering_dict = {int(cid): c for cid, c in req_db["clustering"].items()}
+
+    db = db_interface_sim.db_interface_sim(edge_quads, clustering_dict)
+    return db
+
+
+def form_edge_generator(request, db, wgtr):
+    '''
+    Form the edge generator object. Unlike the database, the generator
+    must be there for the small example / simulator to run.
+    '''
+    try:
+        gen_dict = request['generator']
+    except KeyError:
+        print('Information about the edge generator must be in the request.')
+        sys.exit(1)
+
+    # Get hand-specified results from the verifier that aren't in the
+    # database yet. These are prob_quads of the form (n0, n1, prob,
+    # aug_name).  The weighter will be used to turn the prob into a
+    # weight. 
+    prob_quads = []
+    if 'verifier' in gen_dict:
+        prob_quads = gen_dict['verifier']
+
+    # Get human decisions of the form (n0, n1, bool). These will be
+    # returned as new edges when first requested
+    human_triples = []
+    if 'human' in gen_dict:
+        human_triples = gen_dict['human']
+
+    # Get the ground truth clusters - used to generate edges that
+    # aren't listed explicitly
+    gt_clusters = []
+    if 'gt_clusters' in gen_dict:
+        gt_clusters = gen_dict['gt_clusters']
+
+    # Get the nodes to be removed early in the computation.
+    nodes_to_remove = []
+    if 'nodes_to_remove' in gen_dict:
+        nodes_to_remove = gen_dict['nodes_to_remove']
+
+    # Get the number of steps between returning edge generation
+    # results. If this value is 0 then they are returned immediately
+    # upon request.
+    delay_steps = 0
+    if 'delay_steps' in gen_dict:
+        delay_steps = gen_dict['delay_steps']
+
+    edge_gen = edge_generator_sim.edge_generator_sim(db, wgtr, prob_quads, human_triples,
+                                                     gt_clusters, nodes_to_remove, delay_steps)
+    return edge_gen
+
+
+def extract_requests(request, db):
+    try:
+        req_dict = request['request']
+    except KeyError:
+        print('Information about the GA request itself must be in the request JSON.')
+        sys.exit(1)
+
+    # 1. Get the verifier result quads (n0, n1, prob, aug_name).
+    verifier_results = []
+    if 'verifier' in req_dict:
+        verifier_results = req_dict['verifier']
+
+    # 2. Get the human decision result triples (n0, n1, bool)
+    # No error checking is used
+    human_decisions = []
+    if 'human' in req_dict:
+        human_decisions = req_dict['human']
+
+    # 3. Get the list of existing cluster ids to check
+    cluster_ids_to_check = []
+    if 'cluster_ids' in req_dict:
+        cluster_ids_to_check = req_dict['cluster_ids']
+
+    for cid in cluster_ids_to_check:
+        if not db.cluster_exists(cid):
+            print('GA request cluster id %d does not exist' % cid)
+            raise ValueError('Cluster id does not exist')
+
+    return verifier_results, human_decisions, cluster_ids_to_check
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("overall_driver.py")
+    parser.add_argument("--ga_config", type=str, required=True,
+                        help="graph algorithm config INI file")
+    parser.add_argument("--verifier_gt", type=str, required=True,
+                        help="json file containing verification algorithm ground truth")
+    parser.add_argument("--request", type=str, required=True,
+                        help="json file continain graph algorithm request info")
+    parser.add_argument("--db_result", type=str,
+                        help="file to write resulting json database")
+
+    # 1. Configuration
+    args = parser.parse_args()
+    config_ini = configparser.ConfigParser()
+    config_ini.read(args.ga_config)
+
+    # 2. Recent results from verification ground truth tests. Used to
+    # establish the weighter.
+    fn = open(args.verifier_gt)
+    verifier_gt = json.loads(fn.read())
+    fn.close()
+
+    # 3. Form the parameters dictionary and weight objects (one per
+    # verification algorithm).
+    ga_params, wgtrs = ga_driver.params_and_weighters(config_ini, verifier_gt)
+    if len(wgtrs) > 1:
+        print('Not currently handling more than one weighter!!')
+        sys.exit(1)
+    wgtr = wgtrs[0]
+
+    # 4. Get the request dictionary, which includes the database, the
+    # actual request edges and clusters, and the edge generator edges
+    # and ground truth (for simulation).
+    fn = open(args.request)
+    request = json.loads(fn.read())
+    fn.close()
+    db = form_database(request)
+    edge_gen = form_edge_generator(request, db, wgtr)
+    verifier_req, human_req, cluster_req = extract_requests(request, db)
+
+    # 5. Form the graph algorithm driver
+    driver = ga_driver.ga_driver(verifier_req, human_req, cluster_req, db,
+                                 edge_gen, ga_params)
+
+    # 6. Run it. Changes are logged.
+    changes_to_review = driver.run_all_ccPICs()
+
+    # 7. Commit changes. Record them in the database and the log
+    # file.
+    # TBD
+    
