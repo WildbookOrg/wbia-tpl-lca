80295f4d2f753515b9cedef961817c6fd6a97f55
Jason Parham <bluemellophone@gmail.com>
2020-10-21 15:08:08 -0700

    Updated code for wbia namespace instead of ibeis

--- __init__.py
+++ __init__.py
@@ -1,24 +1,24 @@
 # -*- coding: utf-8 -*-
 try:
-    from ibeis_lca._version import __version__
+    from wbia_lca._version import __version__
 except ImportError:
     __version__ = '0.0.0'
 
-# from ibeis_lca.version import version as __version__  # NOQA
-from ibeis_lca import _plugin  # NOQA
+# from wbia_lca.version import version as __version__  # NOQA
+from wbia_lca import _plugin  # NOQA
 
-from ibeis_lca import baseline  # NOQA
-from ibeis_lca import cid_to_lca  # NOQA
-from ibeis_lca import cluster_tool  # NOQA
-from ibeis_lca import exp_scores  # NOQA
-from ibeis_lca import graph_algorithm  # NOQA
-from ibeis_lca import lca  # NOQA
-from ibeis_lca import lca_alg1  # NOQA
-from ibeis_lca import lca_alg2  # NOQA
-from ibeis_lca import lca_heap  # NOQA
-from ibeis_lca import lca_queues  # NOQA
-from ibeis_lca import run_from_simulator  # NOQA
-from ibeis_lca import simulato  # NOQA
-from ibeis_lca import test_cluster_tools  # NOQA
-from ibeis_lca import version  # NOQA
-from ibeis_lca import weighter  # NOQA
+from wbia_lca import baseline  # NOQA
+from wbia_lca import cid_to_lca  # NOQA
+from wbia_lca import cluster_tool  # NOQA
+from wbia_lca import exp_scores  # NOQA
+from wbia_lca import graph_algorithm  # NOQA
+from wbia_lca import lca  # NOQA
+from wbia_lca import lca_alg1  # NOQA
+from wbia_lca import lca_alg2  # NOQA
+from wbia_lca import lca_heap  # NOQA
+from wbia_lca import lca_queues  # NOQA
+from wbia_lca import run_from_simulator  # NOQA
+from wbia_lca import simulato  # NOQA
+from wbia_lca import test_cluster_tools  # NOQA
+from wbia_lca import version  # NOQA
+from wbia_lca import weighter  # NOQA
--- _plugin.py
+++ _plugin.py
@@ -1,17 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function
-from ibeis.control import controller_inject
-from ibeis.constants import CONTAINERIZED, PRODUCTION  # NOQA
+from wbia.control import controller_inject
+from wbia.constants import CONTAINERIZED, PRODUCTION  # NOQA
 import utool as ut
-from ibeis_lca import graph_algorithm
+
+import argparse
+import configparser
+import json
+import sys
+
+from wbia_lca import ga_driver
+from wbia_lca.overall_driver import (form_database, form_edge_generator, extract_requests)
 
 (print, rrr, profile) = ut.inject2(__name__)
 
 
 _, register_ibs_method = controller_inject.make_ibs_register_decorator(__name__)
 
-register_api = controller_inject.get_ibeis_flask_api(__name__)
-register_route = controller_inject.get_ibeis_flask_route(__name__)
+register_api = controller_inject.get_wbia_flask_api(__name__)
+register_route = controller_inject.get_wbia_flask_route(__name__)
 
 register_preproc_image = controller_inject.register_preprocs['image']
 register_preproc_annot = controller_inject.register_preprocs['annot']
@@ -19,18 +26,18 @@ register_preproc_annot = controller_inject.register_preprocs['annot']
 
 @register_ibs_method
 @register_api('/api/plugin/lca/', methods=['GET'])
-def ibeis_plugin_lca_init(ibs):
+def wbia_plugin_lca_init(ibs):
     r"""
     Create an LCA graph algorithm object
 
     Args:
-        ibs (IBEISController):  ibeis controller object
+        ibs (IBEISController):  wbia controller object
 
     Returns:
         object: graph_algorithm
 
     CommandLine:
-        python -m ibeis_lca._plugin --test-ibeis_lca_hello_world
+        python -m wbia_lca._plugin --test-wbia_lca_hello_world
 
     RESTful:
         Method: GET
@@ -39,12 +46,12 @@ def ibeis_plugin_lca_init(ibs):
 
     Example0:
         >>> # ENABLE_DOCTEST
-        >>> import ibeis
+        >>> import wbia
         >>> import utool as ut
-        >>> from ibeis.init import sysres
+        >>> from wbia.init import sysres
         >>> dbdir = sysres.ensure_testdb_identification_example()
-        >>> ibs = ibeis.opendb(dbdir=dbdir)
-        >>> resp = ibs.ibeis_plugin_lca_init()
+        >>> ibs = wbia.opendb(dbdir=dbdir)
+        >>> resp = ibs.wbia_plugin_lca_init()
         >>>
         >>> # Result is a special variable in our doctests.  If the last line
         >>> # contains a "result" assignment, then the test checks if the lines
@@ -56,7 +63,7 @@ def ibeis_plugin_lca_init(ibs):
         >>>     'annotations' : len(ibs.get_valid_aids()),
         >>>     'names'       : len(ibs.get_valid_nids()),
         >>> })
-        [ibeis_lca] hello world with IBEIS controller <IBEISController(testdb_identification) with UUID 1654bdc9-4a14-43f7-9a6a-5f10f2eaa279>
+        [wbia_lca] hello world with IBEIS controller <IBEISController(testdb_identification) with UUID 1654bdc9-4a14-43f7-9a6a-5f10f2eaa279>
         {
             'annotations': 70,
             'database': UUID('1654bdc9-4a14-43f7-9a6a-5f10f2eaa279'),
@@ -66,17 +73,80 @@ def ibeis_plugin_lca_init(ibs):
         }
     """
     ut.embed()
+
+    parser = argparse.ArgumentParser('overall_driver.py')
+    parser.add_argument(
+        '--ga_config', type=str, required=True, help='graph algorithm config INI file'
+    )
+    parser.add_argument(
+        '--verifier_gt',
+        type=str,
+        required=True,
+        help='json file containing verification algorithm ground truth',
+    )
+    parser.add_argument(
+        '--request',
+        type=str,
+        required=True,
+        help='json file continain graph algorithm request info',
+    )
+    parser.add_argument(
+        '--db_result', type=str, help='file to write resulting json database'
+    )
+
+    # 1. Configuration
+    args = parser.parse_args()
+    config_ini = configparser.ConfigParser()
+    config_ini.read(args.ga_config)
+
+    # 2. Recent results from verification ground truth tests. Used to
+    # establish the weighter.
+    fn = open(args.verifier_gt)
+    verifier_gt = json.loads(fn.read())
+    fn.close()
+
+    # 3. Form the parameters dictionary and weight objects (one per
+    # verification algorithm).
+    ga_params, wgtrs = ga_driver.params_and_weighters(config_ini, verifier_gt)
+    if len(wgtrs) > 1:
+        print('Not currently handling more than one weighter!!')
+        sys.exit(1)
+    wgtr = wgtrs[0]
+
+    # 4. Get the request dictionary, which includes the database, the
+    # actual request edges and clusters, and the edge generator edges
+    # and ground truth (for simulation).
+    fn = open(args.request)
+    request = json.loads(fn.read())
+    fn.close()
+    db = form_database(request)
+    edge_gen = form_edge_generator(request, db, wgtr)
+    verifier_req, human_req, cluster_req = extract_requests(request, db)
+
+    # 5. Form the graph algorithm driver
+    driver = ga_driver.ga_driver(
+        verifier_req, human_req, cluster_req, db, edge_gen, ga_params
+    )
+
+    # 6. Run it. Changes are logged.
+    changes_to_review = driver.run_all_ccPICs()
+
+    # 7. Commit changes. Record them in the database and the log
+    # file.
+    # TBD
+
+
     graph = graph_algorithm.graph_algorithm()  # NOQA
 
     args = (ibs,)
-    resp = '[ibeis_lca] hello world with IBEIS controller %r' % args
+    resp = '[wbia_lca] hello world with WBIA controller %r' % args
     return resp
 
 
 if __name__ == '__main__':
     r"""
     CommandLine:
-        python -m ibeis_lca._plugin --allexamples
+        python -m wbia_lca._plugin --allexamples
     """
     import multiprocessing
 
